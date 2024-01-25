f7429bad07b1cda66dd2b1e119c281ca99709b9e
Zhirun Yan <sa615371@mail.ustc.edu.cn>
2024-01-25 20:15:03 +0800

    fix: 347 top k frequent elements
    Signed-off-by: Zhirun Yan <sa615371@mail.ustc.edu.cn>

--- src/347_top_k_frequent_elements.rs
+++ src/347_top_k_frequent_elements.rs
@@ -8,7 +8,9 @@ pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
     }
 
     let mut mapv: Vec<(&i32, &i32)> = map.iter().collect();
-    mapv.sort_by(|x, y| y.1.cmp(x.1));
+
+    mapv.sort_by(|x, y| y.1.cmp(x.1)); //sort (key, value) by value from big to little
+
     let res = mapv.drain(0..k as usize).map(|(&n, _)| n).collect();
 
     res
@@ -17,12 +19,13 @@ pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
 #[cfg(test)]
 #[test]
 pub fn test_347() {
-    let nums = vec![1,1,11,22,55,23,22,1,25,11];
+    let nums = vec![1, 1, 11, 22, 55, 23, 22, 1, 25, 11];
     let k = 3;
-    assert_eq!(top_k_frequent(nums, k), vec![1, 11, 22]);
+    let mut res = top_k_frequent(nums, k);
+    res.sort();
+    assert_eq!(res, vec![1, 11, 22]);
 
-    let nums = vec![1, 1, 1, 2, 2, 3];
+    let nums = vec![1, 1, 1, 1, 1, 2, 2, 3];
     let k = 2;
     assert_eq!(top_k_frequent(nums, k), vec![1, 2]);
 }
-
